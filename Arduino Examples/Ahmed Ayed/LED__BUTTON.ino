dcd3d0fe1a69fd3180b752aaba1f2e741b60478a
ahmedayd23 <36535924+ahmedayd23@users.noreply.github.com>
2018-02-20 00:00:19 +0200

    Add files via upload

---
+++ Arduino Examples/Ahmed Ayed/LED__BUTTON.ino
@@ -0,0 +1,16 @@
+int led =13;
+int button=0;
+int val;
+void setup() {
+  pinMode(led,OUTPUT);
+  pinMode(button,INPUT);
+}
+
+void loop() {
+  val=digitalRead(button);
+ if ( val==HIGH){
+  digitalWrite(led,HIGH);
+  delay(2000);
+  }
+ else  digitalWrite(led,LOW);
+}
---
+++ Arduino Examples/Ahmed Ayed/traffic.ino
@@ -0,0 +1,76 @@
+  int yellow = 0;
+  int red = 1;
+  int green = 2;
+  
+void setup()
+{
+  pinMode(yellow,OUTPUT);
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(yellow,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(green,HIGH);
+  delay (2000);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,HIGH);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,HIGH);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,HIGH);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,HIGH);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,HIGH);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  delay (200);
+
+  digitalWrite(green,LOW);
+  digitalWrite(yellow,LOW);
+  digitalWrite(red,HIGH);
+  delay (2000);
+
+
+  
+}
