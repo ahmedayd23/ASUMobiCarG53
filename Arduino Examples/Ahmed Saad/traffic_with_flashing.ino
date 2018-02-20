1264b23c074093d8e2a9bdc47d9593d16809040c
ahmedsaad26 <36536734+ahmedsaad26@users.noreply.github.com>
2018-02-20 17:24:33 +0200

    Add files via upload

---
+++ Arduino Examples/Ahmed Saad/traffic_with_flashing.ino
@@ -0,0 +1,47 @@
+int led1=11;
+int led2=12;
+int led3=13;
+void setup() {
+  pinMode(led1,OUTPUT); 
+    pinMode(led2,OUTPUT);
+      pinMode(led3,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(led1,HIGH);
+   digitalWrite(led2,LOW);
+    digitalWrite (led3,LOW);
+    delay(1000);
+    digitalWrite(led1,LOW);
+   digitalWrite(led2,HIGH);
+    digitalWrite (led3,LOW);
+    delay(500);
+    digitalWrite(led1,LOW);
+   digitalWrite(led2,LOW);
+    digitalWrite (led3,LOW);
+    delay(300);
+    digitalWrite(led1,LOW);
+   digitalWrite(led2,HIGH);
+    digitalWrite (led3,LOW);
+    delay(500);
+     digitalWrite(led1,LOW);
+   digitalWrite(led2,LOW);
+    digitalWrite (led3,LOW);
+    delay(300);
+    digitalWrite(led1,LOW);
+   digitalWrite(led2,HIGH);
+    digitalWrite (led3,LOW);
+    delay(500);
+    digitalWrite(led1,LOW);
+   digitalWrite(led2,LOW);
+    digitalWrite (led3,LOW);
+    delay(300);
+     digitalWrite(led1,LOW);
+   digitalWrite(led2,LOW);
+    digitalWrite (led3,HIGH);
+    delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
